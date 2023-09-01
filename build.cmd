f74c541397ab654a6087aa2671ef5f8a9770279f
Lunatixz <128316+Lunatixz@users.noreply.github.com>
2023-09-01 12:24:58 -0400

    Cleanup
    Cleanup
    
    Cleanup
    
    Cleanup
    
    Cleanup

--- build.cmd
+++
@@ -1,7 +0,0 @@
-rd /s /q C:\GitHub\PseudoTV_Live\plugin.video.pseudotv.live\
-cd\
-C:
-cd\Program Files\TeraCopy
-TeraCopy.exe Copy "C:\Program Files\Kodi\portable_data\addons\plugin.video.pseudotv.live" C:\GitHub\PseudoTV_Live\ /OverwriteAll
-cd\GitHub\PseudoTV_Live
-addon_generator.py
--- copy.cmd
+++
@@ -1,9 +0,0 @@
-cd\
-C:
-cd\Program Files\TeraCopy
-rd /s /q \\192.168.0.51\xbmc\portable_data\addons\plugin.video.pseudotv.live\
-TeraCopy.exe Copy "C:\GitHub\PseudoTV_Live\plugin.video.pseudotv.live" \\192.168.0.51\xbmc\portable_data\addons\ /OverwriteAll
-rd /s /q \\192.168.0.123\internal\Android\data\org.xbmc.kodi\files\.kodi\addons\plugin.video.pseudotv.live\
-TeraCopy.exe Copy "C:\GitHub\PseudoTV_Live\plugin.video.pseudotv.live" \\192.168.0.123\internal\Android\data\org.xbmc.kodi\files\.kodi\addons\ /OverwriteAll
-rd /s /q \\192.168.0.124\internal\Android\data\org.xbmc.kodi\files\.kodi\addons\plugin.video.pseudotv.live\
-TeraCopy.exe Copy "C:\GitHub\PseudoTV_Live\plugin.video.pseudotv.live" \\192.168.0.124\internal\Android\data\org.xbmc.kodi\files\.kodi\addons\ /OverwriteAll
