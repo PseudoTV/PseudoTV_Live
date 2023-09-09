0d427dc6c22fdfa269c9ce934ea4dcc510cf5190
Lunatixz <128316+Lunatixz@users.noreply.github.com>
2023-09-09 13:54:53 -0400

    Revert "Cleanup"
    This reverts commit e91ee0100951a3fc522d8beab53351acd8ad3f8e.

---
+++ build.cmd
@@ -0,0 +1,7 @@
+rd /s /q C:\GitHub\PseudoTV_Live\plugin.video.pseudotv.live\
+cd\
+C:
+cd\Program Files\TeraCopy
+TeraCopy.exe Copy "C:\Program Files\Kodi\portable_data\addons\plugin.video.pseudotv.live" C:\GitHub\PseudoTV_Live\ /OverwriteAll
+cd\GitHub\PseudoTV_Live
+addon_generator.py
